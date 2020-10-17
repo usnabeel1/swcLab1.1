da4a4d2f726552b0cddf50791c86be763eb530e2
eddiemoonn <64956566+eddiemoonn@users.noreply.github.com>
2020-10-17 14:51:37 +0300

    Changed output statement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,19 +3,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Constructing a House\n");
+        System.out.println("Construct a House\n");
         PrepareHouse house = new House();
         house.constructHouse();
 
         System.out.println("**********************************************");
 
-        System.out.println("Constructing a Flat\n");
+        System.out.println("Construct a Flat\n");
         house = new Flat();
         house.constructHouse();
 
         System.out.println("**********************************************");
 
-        System.out.println("Constructing a Tenament\n");
+        System.out.println("Construct a Tenament\n");
         house = new Tenament();
         house.constructHouse();
     }
